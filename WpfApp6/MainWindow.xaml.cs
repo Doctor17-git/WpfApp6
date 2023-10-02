ea59afc7f53536f091e561af898584b244f7acef
Doctor 13 <grom4322@gmail.com>
2023-10-02 10:29:05 +0300

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            button1.Content = "Привет!";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            button2.Background = new SolidColorBrush(Colors.Red);
+            button2.Foreground = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
